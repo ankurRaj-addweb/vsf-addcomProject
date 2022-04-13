01d22db53cc98ee4f79ed8ca02fee658ff50b80f
shreyanshi-vekariya <shreyanshi.v.addweb@gmail.com>
2022-04-13 17:49:55 +0530

    Instagram Feed

--- components/InstagramFeed.vue
+++ components/InstagramFeed.vue
@@ -1,103 +1,128 @@
 <template>
-  <SfSection
+  <AwSection
     :title-heading="$t('Share Your Look')"
     subtitle-heading="#YOURLOOK"
     class="title"
   >
     <div class="grid grid-images">
+
       <div class="grid__row">
-        <div class="grid__col">
+        <div class="grid__col container">
           <nuxt-img
             v-if="isMobile"
-            src="/homepage/imageAm.webp"
+            src="/homepage/imageAm.webp "
             alt="katherina_trn"
             loading="lazy"
-            :width="160"
-            :height="160"
+            :width="163"
+            :height="220"
           />
           <nuxt-img
             v-else
-            src="/homepage/imageAd.webp"
+            src="/homepage/img1.png"
+            class="img-opacity"
             alt="katherina_trn"
             loading="lazy"
             :width="470"
             :height="470"
+            
           />
+          <div class="middle">
+            <div class="text">@instagram</div>
+          </div>
         </div>
-        <div class="grid__col small">
+
+        <div class="grid__col small container">
           <nuxt-img
             v-if="isMobile"
             src="/homepage/imageBm.webp"
             alt="katherina_trn"
             loading="lazy"
-            :width="160"
-            :height="160"
+            :width="162"
+            :height="162"
           />
           <nuxt-img
             v-else
-            src="/homepage/imageCd.webp"
+            src="/homepage/img2.png"
+            class="img-opacity"
             alt="katherina_trn"
             loading="lazy"
             :width="470"
             :height="160"
           />
-        </div>
+          <div class="middle">
+            <div class="text">@instagram</div>
+          </div>
       </div>
+        </div>
+        
       <div class="grid__row">
-        <div class="grid__col small">
+        <div class="grid__col small container">
           <nuxt-img
             v-if="isMobile"
             src="/homepage/imageCm.webp"
             alt="katherina_trn"
             loading="lazy"
-            :width="160"
-            :height="160"
+            :width="163"
+            :height="220"
           />
           <nuxt-img
             v-else
-            src="/homepage/imageBd.webp"
+            class="img-opacity"
+            src="/homepage/img3.png"
             alt="katherina_trn"
             loading="lazy"
             :width="470"
             :height="160"
           />
+          <div class="middle">
+            <div class="text">@instagram</div>
+          </div>
         </div>
-        <div class="grid__col">
+        <div class="grid__col container" >
           <nuxt-img
             v-if="isMobile"
             src="/homepage/imageDm.webp"
             alt="katherina_trn"
             loading="lazy"
-            :width="160"
-            :height="160"
+            :width="162"
+            :height="162"
           />
           <nuxt-img
             v-else
-            src="/homepage/imageDd.webp"
+            class="img-opacity"
+            src="/homepage/img4.png"
             alt="katherina_trn"
             loading="lazy"
             :width="470"
             :height="470"
           />
+          <div class="middle">
+            <div class="text">@instagram</div>
+          </div>
         </div>
+
       </div>
+
     </div>
-  </SfSection>
+
+    </AwSection>
 </template>
 <script>
-import {
-  SfSection,
-} from '@storefront-ui/vue';
+import AwSection from "@storefront-ui/root/packages/vue/src/components/molecules/AwSection/AwSection.vue";
 import {
   mapMobileObserver,
   unMapMobileObserver,
-} from '@storefront-ui/vue/src/utilities/mobile-observer.js';
-import { computed, defineComponent, onBeforeUnmount } from '@nuxtjs/composition-api';
+} from "@storefront-ui/vue/src/utilities/mobile-observer.js";
+import {
+  computed,
+  defineComponent,
+  onBeforeUnmount,
+} from "@nuxtjs/composition-api";
 
 export default defineComponent({
-  name: 'InstagramFeed',
+  name: "InstagramFeed",
   components: {
-    SfSection,
+    AwSection,
   },
   setup() {
     const isMobile = computed(() => mapMobileObserver().isMobile.get());
@@ -149,8 +174,8 @@ export default defineComponent({
       &.small {
         height: 10rem;
       }
-     width: 29.375rem;
-     height: 29.375rem;
+      width: 29.375rem;
+      height: 29.375rem;
     }
     & + & {
       margin-top: var(--spacer-xs);
@@ -160,5 +185,40 @@ export default defineComponent({
     }
   }
 }
+.img-opacity:hover{
+  opacity: 0.6;
+}
+.grid__col {
+  background-color: #282828;
+  
+}
+.container {
+  position: relative;
+  
+}
 
+
+
+.middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+
+.container:hover .middle {
+  opacity: 0.6;
+}
+
+.text {
+  
+  color: #FFFFFF;
+  font-size: 16px;
+  padding: 16px 32px;
+}
 </style>
