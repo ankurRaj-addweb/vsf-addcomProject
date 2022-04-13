1df66c35fb4e54ee995bd8f2027ccc2e2addb969
mansiaddweb <mansi.s.addweb@gmail.com>
2022-04-13 17:46:04 +0530

    AppHeader and TopBar

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,32 +1,35 @@
 <template>
-  <SfTopBar class="topbar">
+  <AwTopBar class="topbar">
     <template #left>
-      <SfButton class="sf-button--text">
+      <AwButton class="sf-button--text" style="width:82px; color:#037EE6">
         {{ $t('Help & FAQs') }}
-      </SfButton>
+      </AwButton>
     </template>
     <template #center>
-      <p>{{ $t('Download') }}</p>
-      <SfButton class="topbar__button sf-button--text">
+      <p>{{ $t('Download our application. ') }}</p>
+      <AwButton class="topbar__button sf-button--text">
         {{ $t('Find out more') }}
-      </SfButton>
+      </AwButton>
     </template>
     <template #right>
       <CurrencySelector />
       <StoreSwitcher />
     </template>
-  </SfTopBar>
+  </AwTopBar>
 </template>
 
 <script>
 import { SfButton, SfTopBar } from '@storefront-ui/vue';
 import CurrencySelector from './CurrencySelector';
 import StoreSwitcher from './StoreSwitcher';
-
+import AwButton from '@storefront-ui/root/packages/vue/src/components/atoms/AwButton/AwButton.vue';
+import AwTopBar from '@storefront-ui/root/packages/vue/src/components/organisms/AwTopBar/AwTopBar.vue';
 export default {
   components: {
     CurrencySelector,
     SfTopBar,
+    AwButton,
+    AwTopBar,
     SfButton,
     StoreSwitcher,
   },
@@ -40,5 +43,23 @@ export default {
   &__button {
     margin: 0 0 0 var(--spacer-xs);
   }
+}
+.sf-button--text{
+  width: 92px;
+  height: 22px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18px;
+  // font-family: Source Sans Pro;
+}
+p{
+  width: 178px;
+  height: 22px;
+  left: 580px;
+  top: 9px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22px;
+  // font-family: Source Sans Pro;
 }
 </style>
