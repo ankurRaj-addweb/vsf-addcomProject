a983c65ca45ab170b5c60a37f0fe347bd0482bc8
addweb <aditya@addwebsolution.in>
2022-05-18 18:33:15 +0530

    update SCSS and VSFshippingprovider

--- components/Checkout/VsfShippingProvider.vue
+++ components/Checkout/VsfShippingProvider.vue
@@ -30,6 +30,7 @@
         </div>
       </AwLoader>
       <div class="shipping-method">
+        <div class="free-shipping">
           <AwRadio
             class="sf-radio--transparent"
             name="Shipping"
@@ -41,6 +42,8 @@
             selected=""
             :required="false"
           />
+           <div><p class="free">Free</p></div>
+          </div>
           <button
             class="accordion"
             :class="{ active: showCategoryA }"
@@ -49,10 +52,10 @@
             @click="showCategoryA = !showCategoryA"
             type="link"
           >
-            info
+            info   
+            
           </button>
           <ul class="category-list" v-show="showCategoryA">
-            <li>
               <p>
                 A paragraph is a series of related sentences developing a
                 central idea, called the topic. Try to think about paragraphs in
@@ -60,9 +63,9 @@
                 sentences that supports one central, unified idea. Paragraphs
                 add one idea at a time to your broader argument.
               </p>
-            </li>
           </ul>
       </div>
+     
       <div class="shipping-method">
          <AwRadio
           class="sf-radio--transparent"
@@ -75,6 +78,25 @@
           selected=""
           :required="false"
         />  
+         <button
+            class="accordion"
+            :class="{ active: showCategoryB }"
+            :aria-disabled="false"
+            :link="null"
+            @click="showCategoryB = !showCategoryB"
+            type="link"
+          >
+            info
+          </button>
+          <ul class="category-list" v-show="showCategoryB">
+              <p>
+                A paragraph is a series of related sentences developing a
+                central idea, called the topic. Try to think about paragraphs in
+                terms of thematic unity: a paragraph is a sentence or a group of
+                sentences that supports one central, unified idea. Paragraphs
+                add one idea at a time to your broader argument.
+              </p>
+          </ul>
       </div>
       <div class="shipping-method">
         <AwRadio
@@ -88,6 +110,25 @@
           selected=""
           :required="false"
         />
+         <button
+            class="accordion"
+            :class="{ active: showCategoryC }"
+            :aria-disabled="false"
+            :link="null"
+            @click="showCategoryC = !showCategoryC"
+            type="link"
+          >
+            info
+          </button>
+            <ul class="category-list" v-show="showCategoryC">
+              <p>
+                A paragraph is a series of related sentences developing a
+                central idea, called the topic. Try to think about paragraphs in
+                terms of thematic unity: a paragraph is a sentence or a group of
+                sentences that supports one central, unified idea. Paragraphs
+                add one idea at a time to your broader argument.
+              </p>
+          </ul>
       </div>
       <div class="shipping-method">
         <AwRadio
@@ -101,6 +142,25 @@
           selected=""
           :required="false"
         />
+         <button
+            class="accordion"
+            :class="{ active: showCategoryD }"
+            :aria-disabled="false"
+            :link="null"
+            @click="showCategoryD = !showCategoryD"
+            type="link"
+          >
+            info
+          </button>
+          <ul class="category-list" v-show="showCategoryD">
+              <p>
+                A paragraph is a series of related sentences developing a
+                central idea, called the topic. Try to think about paragraphs in
+                terms of thematic unity: a paragraph is a sentence or a group of
+                sentences that supports one central, unified idea. Paragraphs
+                add one idea at a time to your broader argument.
+              </p>
+          </ul>
       </div>
       <div class="shipping-method">
         <AwRadio
@@ -114,7 +174,28 @@
           selected=""
           :required="false"
         />
+         <button
+            class="accordion"
+            :class="{ active: showCategoryE }"
+            :aria-disabled="false"
+            :link="null"
+            @click="showCategoryE = !showCategoryE"
+            type="link"
+          >
+            info
+          </button>
+           <ul class="category-list" v-show="showCategoryE">
+              <p>
+                A paragraph is a series of related sentences developing a
+                central idea, called the topic. Try to think about paragraphs in
+                terms of thematic unity: a paragraph is a sentence or a group of
+                sentences that supports one central, unified idea. Paragraphs
+                add one idea at a time to your broader argument.
+              </p>
+          </ul>
+          
       </div>
+      
       <div class="shipping-method">
         <AwRadio
           v-for="(method, index) in shippingMethods"
@@ -148,7 +229,7 @@
         </AwRadio> 
       </div>
       </div>
-      <!-- <div class="form__action">
+      <div class="form__action">
         <AwButton
           v-e2e="'continue-to-billing'"
           class="form__action-button"
@@ -157,7 +238,7 @@
         >
           {{ $t("Go To Shipping") }}
         </AwButton>
-      </div> -->
+      </div>
     </div>
 </template>
 <script>
@@ -172,7 +253,7 @@ import AwButton from "@storefront-ui/root/packages/vue/src/components/atoms/AwBu
 import AwRadio from "@storefront-ui/root/packages/vue/src/components/molecules/AwRadio/AwRadio.vue";
 import AwLoader from "@storefront-ui/root/packages/vue/src/components/atoms/AwLoader/AwLoader.vue";
 
-import { computed, defineComponent } from "@nuxtjs/composition-api";
+import { computed, defineComponent, ref } from "@nuxtjs/composition-api";
 import getShippingMethodPrice from "~/helpers/checkout/getShippingMethodPrice";
 
 export default defineComponent({
@@ -190,6 +271,11 @@ export default defineComponent({
       error: errorUseGetShippingMethods,
     } = useGetShippingMethods();
     const { cart } = useCart();
+    const showCategoryA = ref(false)
+    const showCategoryB = ref(false)
+    const showCategoryC = ref(false)
+    const showCategoryD = ref(false)
+    const showCategoryE = ref(false)
     const {
       state,
       save: saveShippingProvider,
@@ -234,6 +320,13 @@ export default defineComponent({
       shippingMethods,
       state,
       totals,
+    showCategoryA,
+    showCategoryB,
+    showCategoryC,
+    showCategoryD,
+    showCategoryE,
+
+
     };
   },
 });
@@ -323,7 +416,10 @@ button.form__action-button.sf-button {
     font-size: 15px;
   }
 }
-
+.free {
+    margin-left: 650px;
+    margin-top: -50px;
+}
 .categoryBrowser ul {
   list-style-type: none;
 }
@@ -388,7 +484,7 @@ button.accordion {
   button.accordion {
     position: absolute;
     top: 0;
-    left: 38%;
+    left: 27%;
     background: transparent;
     text-decoration: underline;
     color: #282828;
@@ -396,6 +492,11 @@ button.accordion {
     font-weight: 400;
     font-size: 14px;
     line-height: 18px;
+  }
+  .free-shipping{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
 }
 </style>
